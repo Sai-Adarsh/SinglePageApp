941034e89eadd6f6dc29eae23ba7655b04434b3e
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2020-10-31 02:13:48 +0530

    firsT

--- app/src/main/java/com/example/singlepageapp/MainActivity.kt
+++ app/src/main/java/com/example/singlepageapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.singlepageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            sendMessage()
+        }
     }
+
+    private fun sendMessage() {
+        Toast.makeText(this@MainActivity, "Its toast!", Toast.LENGTH_SHORT).show();
+    }
+
 }
